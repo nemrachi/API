a8040442f571d2c2ca201e7e255cd51a3d331c95
Ema Richnakova <ema.richnakova@gmail.com>
2022-02-18 00:58:48 +0100

    ul1-5 done

--- blok1/ul1-5/ul1-5.c
+++ blok1/ul1-5/ul1-5.c
@@ -1,14 +1,51 @@
 #include "ul1-5.h"
 
-float cosine(float radians, float epsilon) {
+float absolute(float num) {
+    return (num >= 0) ? num : -num;
+}
 
+float modulo(float a, float b) {
+    a = (a < 0) ? -a : a;
+    return ((a/b) - (int)(a/b))*b;
+}
+
+float power(float base, float power) {
+    if (power == 0) {
+        return 1;
+    }
+    float result = base;
+    for (int i = 0; i < power-1; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+float factorial(float number) {
+    float result = 1;
+    while(number > 1) {
+        result *= number;
+        number--;        
+    }
+    return result;
+}
+
+float cosine(float degrees, float epsilon) {
+    float cos = 0;
+    float tmp = cos;
+    float n = 0;
+    float radians = modulo(degrees, 360.0) * (PI / 180.0);
+
+    while(1) {
+        tmp = cos;
+        cos += (power(-1, n)*power(radians, 2*n))/factorial(2*n);
+        if (absolute(cos - tmp) < epsilon) {
+            return cos;
+        }
+        n++;
+    }
 }
 
 int main(void) {
-    int k;
-       
-    printf("Enter number: ");
-    scanf("%d", &k);
-
+    printf("%f", cosine(8649, EPSILON));
     return 0;
 }
--- blok1/ul1-5/ul1-5.h
+++ blok1/ul1-5/ul1-5.h
@@ -1,3 +1,8 @@
 #include <stdio.h>
 
-float cosine(float radians, float epsilon);
+static const float EPSILON = 0.000001;
+static const float PI = 3.14159265358979323846;
+float modulo(float a, float b);
+float power(float base, float power);
+float factorial(float number);
+float cosine(float degrees, float epsilon);
